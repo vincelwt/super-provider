0e32d6c7da1a2a58df801a42706eb8a1dabd3993
Vince Loewe <vincent.loewert@gmail.com>
2022-11-08 19:43:26 +0800

    😎 Super Provider

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
-# fallthrough-provider
+# super-provider
 
-Resilient provider for Ethers.js that will switch to the next provider in the list if the current is too slow or fails.
+Meta-provider for Ethers.js engineered for optimal speed, reliability and network usage.
 
-Inspired by essential-eth's [fallthrough provider](https://github.com/dawsbot/essential-eth/blob/master/src/providers/FallthroughProvider.ts).
+Best used with many sub providers.
 
-Great if you use unreliable or public RPCs or need a provider that never goes down.
+### How it works
 
-Supports listening to events with `.on()`.
+- It will regularly benchmark (with `getBlockNumber`) the providers supplied and rank them by response time. Providers lagging in blocks won't be used.
+- Depending on the selected mode: Calls are either:
+  - spread between the top 3 fastest providers (default)
+  - send to the fastest providers in parallel and the first response is returned (fastest)
+- If any call fails or stalls, it is automatically retried on another provider.
 
 ## Installation
 
 ```bash
-npm install fallthrough-provider
+npm install super-provider
 ```
 
 ## Example Usage
 
 ```typescript
-import { FallthroughProvider } from 'fallthrough-provider';
+import { SuperProvider } from 'super-provider';
 
 import { ethers } from 'ethers'
 
@@ -28,10 +32,38 @@ const rpcs = [
   "https://rpc.ankr.com/eth"
 ]
 
-const provider = new FallthroughProvider(rpcs.map(url => new ethers.providers.JsonRpcProvider(url)))
+const provider = new SuperProvider(rpcs.map(url => new ethers.providers.JsonRpcProvider(url)))
 
 const blockNumber = await provider.getBlockNumber()
 console.log(blockNumber)
 ```
+
+## API
+
+### `new SuperProvider(providers: Provider[], options?: SuperProviderOptions)`
+- `providers`: Array of providers to use
+- `options`: Options for the super provider
+
+### `options`
+- `mode`: Mode to use for calls. Either `spread` or `parallel`. Default: `spread`
+- `maxRetries`: Maximum number of retries for a call. Default: `3`
+- `benchmarkFrequency`: Interval between benchmarks. Default: `300000` (5 minutes)
+- `acceptableBlockLag`: Maximum number of blocks a provider can lag behind the fastest provider. Default: `0`
+- `benchmarkRuns`: Number of runs for each benchmark. Default: `3`
+- `stallTimeout`: Timeout for a call to be considered stalled. Default: `4000` (4 seconds)
+- `maxParallel`: Maximum number of parallel calls. Default: `3`
+
+
+## Caveats
+
+- Currently doesn't support subscriptions & listening to events with `.on()`. PR welcome.
+- Doesn't have a concept of quorum and doesn't compare results.
+- Lots of untested edge cases.
+
+## Credit
+
+Inspiration taken from ethers's [FallbackProvider](https://docs.ethers.io/v5/api/providers/other/), essential-eth's [FallthroughProvider](https://github.com/dawsbot/essential-eth/blob/master/src/providers/FallthroughProvider.ts) and Chainlist's [auto RPC benchmarking](https://chainlist.org/chain/1).
+
+## Used by
 
 Used by [alt0.io](https://alt0.io) to provide highly reliable and fast chain listening.
--- index.ts
+++ index.ts
@@ -1,98 +1,240 @@
 import { ethers } from 'ethers'
 
-// Takes providers as argument and point to one that works.
-// On error, cycles through providers until one works consistently.
-// If error with the provider or a call takes more than stallTimeout, cycle to the next provider and try again the call
+// https://advancedweb.hu/how-to-add-timeout-to-a-promise-in-javascript/
+const promiseTimeout = (prom: Promise<any>, time: number) =>
+  Promise.race([
+    prom,
+    new Promise((_r, reject) => setTimeout(() => reject('Promise timed out'), time))
+  ])
 
-export class FallthroughProvider extends ethers.providers.BaseProvider {
+const shuffleArray = (array: any[]) =>
+  array
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+
+const quantile = (arr, q) => {
+  const sorted = arr.sort((a, b) => a - b)
+  const pos = (sorted.length - 1) * q
+  const base = Math.floor(pos)
+  const rest = pos - base
+  if (sorted[base + 1] !== undefined) {
+    return sorted[base] + rest * (sorted[base + 1] - sorted[base])
+  } else {
+    return sorted[base]
+  }
+}
+
+type SuperProviderOptions =
+  | {
+      stallTimeout?: number
+      maxRetries?: number
+      acceptableBlockLag?: number
+      benchmarkRuns?: number
+      benchmarkFrequency?: number
+      mode?: 'spread' | 'parallel'
+      maxParallel?: number
+    }
+  | undefined
+
+export class SuperProvider extends ethers.providers.BaseProvider {
   private readonly providers: ethers.providers.BaseProvider[]
-  private currentProviderIndex = 0
-  private currentProvider: ethers.providers.BaseProvider
-  private activeListeners: { [event: string]: ethers.providers.Listener } = {}
-  private stallTimeout: number = 1000
-  private timeout: any
 
-  constructor (providers: ethers.providers.BaseProvider[], stallTimeout = 1000) {
+  private cycleIndex: number = 0
+  private providersPool: { score: number; provider: ethers.providers.BaseProvider }[]
+
+  private stallTimeout: number
+  private maxRetries: number
+  private benchmarkRuns: number
+  private acceptableBlockLag: number
+  private benchmarkFrequency: number
+  private maxParallel: number
+  private mode: 'spread' | 'parallel'
+
+  constructor (providers: ethers.providers.BaseProvider[], options?: SuperProviderOptions) {
     super(providers[0].network)
 
+    this.providersPool = []
     this.providers = providers
-    this.stallTimeout = stallTimeout
-    this.currentProvider = providers[0]
+
+    if (providers.length < 1) {
+      throw new Error('SuperProvider requires at least 1 sub-providers to work.')
+    }
+
+    if (providers.length < 3) {
+      console.warn(
+        'SuperProvider needs at least 3 sub-providers to be effective. The more the better.'
+      )
+    }
+
+    this.stallTimeout = options?.stallTimeout || 4000 // 4s
+    this.maxRetries = options?.maxRetries || 3
+    this.benchmarkRuns = options?.benchmarkRuns || 3
+    this.acceptableBlockLag = options?.acceptableBlockLag || 0
+    this.benchmarkFrequency = options?.benchmarkFrequency || 300000 // 5 minutes
+    this.mode = options?.mode || 'spread'
+    this.maxParallel = options?.maxParallel || 3
+
+    setInterval(() => {
+      this.benchmarkProviders()
+    }, this.benchmarkFrequency)
   }
 
   async detectNetwork (): Promise<ethers.providers.Network> {
-    return this.currentProvider.detectNetwork()
+    return this.providers[0].detectNetwork()
   }
 
-  // if a call fails or takes more than stallTimeout, cycle to the next provider and try again the call
+  // if mode is spread, cycle through top providers. Otherwise make request to all top in parallel.
+  // retry up to maxAttempts times
   async perform (method: string, params: any): Promise<any> {
-    // console.log('Performing', method, params)
-
-    if (this.timeout) {
-      clearTimeout(this.timeout)
+    if (!this.providersPool.length) {
+      await this.benchmarkProviders()
     }
 
-    const cycleAndRetry = () => {
-      console.log(`Retrying ${method}`)
-      clearTimeout(this.timeout)
-      this.cycleProvider()
-      this.timeout = null
-      this.perform(method, params)
+    const promiseGen = p => promiseTimeout(p.perform(method, params), this.stallTimeout)
+
+    let tries = 0
+
+    const recursiveRetry = async (): Promise<any> => {
+      const providers = this.providersToUse()
+
+      try {
+        if (this.mode === 'spread') {
+          // mode "spread" = cycle through fastest providers
+          const provider = providers[this.cycleIndex]
+          this.cycleIndex = (this.cycleIndex + 1) % providers.length
+
+          return await promiseGen(provider)
+        } else {
+          // mode "parallel" = run in parallel and return as soon as one succeeds
+          return await Promise.any(providers.map(promiseGen))
+        }
+      } catch (error) {
+        if (tries >= this.maxRetries) throw error
+
+        tries++
+
+        recursiveRetry()
+      }
     }
 
-    this.timeout = setTimeout(() => {
-      console.log(`Provider timed out doing ${method}.`)
-      cycleAndRetry()
-    }, this.stallTimeout)
-
-    try {
-      const result = await this.currentProvider.perform(method, params)
-      clearTimeout(this.timeout)
-      this.timeout = null
-
-      return result
-    } catch (err) {
-      console.log(`Provider errored doing ${method}.`)
-      cycleAndRetry()
-      // throw err
-    }
+    return recursiveRetry()
   }
 
-  cycleProvider (): void {
-    console.log('Cycling provider')
-    this.removeAllListeners()
-
-    this.currentProviderIndex = (this.currentProviderIndex + 1) % this.providers.length
-    this.currentProvider = this.providers[this.currentProviderIndex]
-
-    // Re-attach listeners to new provider
-    Object.keys(this.activeListeners).forEach(eventName => {
-      this.currentProvider.on(eventName, this.activeListeners[eventName])
-    })
-
-    // Listen to error events on new provider
-    this.currentProvider.on('error', err => {
-      console.log('Error emitted by provider', err)
-      this.cycleProvider()
-    })
-  }
-
-  on (eventName: string, listener: ethers.providers.Listener): this {
-    if (!this.activeListeners[eventName]) {
-      // Save listener to re-attach it when cycling providers
-      this.activeListeners[eventName] = listener
-
-      // Attach to current
-      this.currentProvider.on(eventName, listener)
+  async benchmarkProviders (): Promise<void> {
+    if (this.providers.length <= 2) {
+      this.providersPool = this.providers.map(provider => ({ score: 1, provider }))
+      return
     }
 
-    return this
+    // run benchmark n times and sort by the average response time (lower is better).
+    // downrank providers with blockNumber less than acceptableBlockDeviation
+
+    const run = async () => {
+      // shuffle providers to avoid bias
+      const promises = shuffleArray(this.providers).map(async provider => {
+        const startTime = Date.now()
+        try {
+          const call = provider.perform('getBlockNumber', {})
+          const blockHex = await promiseTimeout(call, this.stallTimeout)
+          const block = parseInt(blockHex, 16)
+          const responseTime = Date.now() - startTime
+          return { provider, responseTime, block }
+        } catch (error) {
+          // @ts-ignore
+          // console.error('Provider failed in benchmark', provider?.connection?.url)
+          return null
+        }
+      })
+
+      const results = (await Promise.all(promises)).filter(p => p) as {
+        provider: ethers.providers.BaseProvider
+        responseTime: number
+        block: number
+      }[]
+
+      // take 3rd quartile of blockHeights as current block height to avoid outliers
+      const currentBlock = quantile(
+        results.map(p => p.block),
+        0.75
+      )
+
+      const filteredResults = results.filter(p => currentBlock - p.block <= this.acceptableBlockLag)
+
+      return filteredResults
+    }
+
+    // run benchmark n times sequentially
+    const results = [] as any[]
+    for (let i = 0; i < this.benchmarkRuns; i++) {
+      results.push(await run())
+    }
+
+    // group results by provider, calc avg response time and sort by it
+    const sortedResults = results
+      .flat()
+      .reduce(
+        (acc, result) => {
+          const existing = acc.find(r => r.provider === result.provider)
+          if (existing) {
+            existing.responseTime += result.responseTime
+            existing.count++
+          } else {
+            acc.push({ ...result, count: 1 })
+          }
+          return acc
+        },
+
+        [] as { provider: ethers.providers.BaseProvider; responseTime: number; count: number }[]
+      )
+      .filter(r => r.count === this.benchmarkRuns)
+      .map(r => ({ ...r, avgResponseTime: r.responseTime / r.count }))
+      .sort((a, b) => a.avgResponseTime - b.avgResponseTime)
+
+    console.log(
+      `benchmark x${this.benchmarkRuns} finished. sorted providers:`,
+      sortedResults.map(
+        // @ts-ignore
+        p => `${p.provider?.connection?.url} | avg ${parseInt(p.avgResponseTime)}ms`
+      )
+    )
+
+    if (!sortedResults.length) {
+      throw new Error('No healthy providers available.')
+    }
+
+    if (sortedResults.length < 3) {
+      console.warn(
+        `Only ${sortedResults.length} healthy providers available. Add more providers for robustness.`
+      )
+    }
+
+    this.providersPool = sortedResults.map(r => ({
+      provider: r.provider,
+      score: 1 / r.avgResponseTime // higher is better
+    }))
+
+    // reset cycle index
+    this.cycleIndex = 0
   }
 
-  removeAllListeners (eventName?: string): this {
-    // remove all listeners from all providers
-    this.providers.forEach(provider => provider.removeAllListeners(eventName))
+  // if raceMode > 0, return top n providers, otherwise return providers with score above median
+  providersToUse (): ethers.providers.BaseProvider[] {
+    if (this.providersPool.length <= 2) {
+      return this.providersPool.map(p => p.provider)
+    }
 
-    return this
+    const sorted = this.providersPool.sort((a, b) => b.score - a.score)
+
+    if (this.mode === 'parallel') {
+      return sorted.slice(0, this.maxParallel).map(p => p.provider)
+    }
+
+    const q2 = quantile(
+      sorted.map(p => p.score),
+      0.5
+    )
+
+    return sorted.filter(p => p.score >= q2).map(p => p.provider)
   }
 }
